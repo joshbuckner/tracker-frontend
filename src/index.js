264adf3a432c7bc2a78357232f2d2bf16c32a5f7
Josh Buckner <joshgbuckner@gmail.com>
2019-07-14 17:14:48 -0500

    front end setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Route, NavLink, BrowserRouter as Router, Switch } from 'react-router-dom'
+import './index.scss'
+import App from './App/App'
+import Contact from './containers/Contact/Contact'
+import Users from './containers/Users/Users'
+import NotFound from './containers/NotFound/NotFound'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+	<Router>
+		<div>
+			<ul>
+        <li>
+          <NavLink exact activeClassName="active" to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="active" to="/users">
+            Users
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="active" to="/contact">
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+			<Switch>
+				<Route exact path='/' component={App} />
+				<Route path='/users' component={Users} />
+				<Route path='/contact' component={Contact} />
+				<Route component={NotFound} />
+			</Switch>
+		</div>
+	</Router>
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(routing, document.getElementById('root'))
